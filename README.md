d2eec0d564eafb6871b304fa7e0e95d2eaafbe10
Bekir Dogan <bekirdo@gmail.com>
2017-04-24 02:33:58 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+```
+docker run --rm -it -e MARATHON_SERVERS=http://172.17.0.2:8080 bergerx/marathon-event-logger
+```
---
+++ marathon_event_logger.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import os
+from marathon import MarathonClient
+
+MARATHON_SERVERS = os.environ.get('MARATHON_SERVERS', 'http://master.mesos:8080')
+AUTH_TOKEN = os.environ.get('MARATHON_AUTH_TOKEN', None)
+USERNAME = os.environ.get('MARATHON_USERNAME', None)
+PASSWORD = os.environ.get('MARATHON_PASSWORD', None)
+
+c = MarathonClient(
+        MARATHON_SERVERS.split(','),
+        username=USERNAME,
+        password=PASSWORD,
+        auth_token=AUTH_TOKEN)
+
+for event in c.event_stream():
+    print event.to_json()
